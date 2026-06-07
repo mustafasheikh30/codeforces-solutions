add634f7d463507391d2b96f4540abc300963334
Mustafa <mustafasheikh188@gmail.com>
2026-06-07 18:09:55 +0530

    added to codeforces

---
+++ contest/255/a/GregsWorkout.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class GregsWorkout {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int chest = 0;
+        int biceps = 0;
+        int back = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int x = sc.nextInt();
+
+            if (i % 3 == 0) {
+                chest += x;
+            } else if (i % 3 == 1) {
+                biceps += x;
+            } else {
+                back += x;
+            }
+        }
+
+        if (chest > biceps && chest > back) {
+            System.out.println("chest");
+        } else if (biceps > chest && biceps > back) {
+            System.out.println("biceps");
+        } else {
+            System.out.println("back");
+        }
+    }
+}
